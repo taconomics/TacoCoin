78d05b78854f0661cc860304cf8ab09b932c9346
Onchained <onchained@ethmail.cc>
2020-11-09 17:34:15 -0800

    comments

--- contracts/pools/core/NFTStakeablePool.sol
+++ contracts/pools/core/NFTStakeablePool.sol
@@ -33,36 +33,16 @@ contract NFTStakeablePool is StakeableToken, RedeemableNFT, Ownable {
     poolName = _poolName;
   }
 
+  /**
+   * Owner functions
+   */
+
   function setMaximumStake(uint256 _stakeSize) public onlyOwner {
     maximumStake = _stakeSize;
   }
 
   function setStakeableStrategyAddress(address _stakeableStrategyAddress) public onlyOwner {
     _setStakeableStrategy(_stakeableStrategyAddress);
-  }
-
-  function _newlyEarnedPoints(address account) private view returns (uint256) {
-    return now.sub(lastUpdateTime(account))
-      .mul(1e18)
-      .div(86400)
-      .mul(balanceOf(account).div(1e8));
-  }
-
-  function earnedPoints(address account) public view returns (uint256) {
-    return points[account].add(_newlyEarnedPoints(account));
-  }
-
-  function stake(uint256 amount) public updatePoints(msg.sender) {
-    require(
-      amount.add(balanceOf(msg.sender)) <= maximumStake,
-      "Cannot stake more tokens"
-    );
-
-    _stake(amount);
-  }
-
-  function withdraw(uint256 amount) public updatePoints(msg.sender) {
-    _withdraw(amount);
   }
 
   function addNFT(
@@ -75,6 +55,38 @@ contract NFTStakeablePool is StakeableToken, RedeemableNFT, Ownable {
 
   function updateNFTStrategy(uint256 nftId, address strategy) public onlyOwner {
     _updateNFTStrategy(nftId, strategy);
+  }
+
+  /**
+   * Points calculations
+   */
+
+  function _newlyEarnedPoints(address account) private view returns (uint256) {
+    return now.sub(lastUpdateTime(account))
+      .mul(1e18)
+      .div(86400)
+      .mul(balanceOf(account).div(1e8));
+  }
+
+  function earnedPoints(address account) public view returns (uint256) {
+    return points[account].add(_newlyEarnedPoints(account));
+  }
+
+  /**
+   * Stakeable and Redeemable functions
+   */
+
+  function stake(uint256 amount) public updatePoints(msg.sender) {
+    require(
+      amount.add(balanceOf(msg.sender)) <= maximumStake,
+      "Cannot stake more tokens"
+    );
+
+    _stake(amount);
+  }
+
+  function withdraw(uint256 amount) public updatePoints(msg.sender) {
+    _withdraw(amount);
   }
 
   function exit() external {
